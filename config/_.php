d8dba8cb84118b9c992689d67760a65247afadf8
Joris <jote0002@stud.kea.dk>
2026-04-27 14:15:26 +0200

    added uuid without dashes function, for future use

--- config/_.php
+++ config/_.php
@@ -7,6 +7,18 @@ function _($text)
 }
 
 // ##############################
+// Stolen from https://stackoverflow.com/questions/2040240/php-function-to-generate-v4-uuid
+function uuidv4_nodash()
+{
+    $data = random_bytes(16);
+
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+
+    return vsprintf('%s%s%s%s%s%s%s%s', str_split(bin2hex($data), 4));
+}
+
+// ##############################
 define("user_email_min", 5);
 define("user_email_max", 50);
 function _validate_user_email()
