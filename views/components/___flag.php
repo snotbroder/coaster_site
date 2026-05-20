156865d27afaaf4b018dab47352567b7ac510d0b
Joris <jote0002@stud.kea.dk>
2026-05-20 10:30:58 +0200

    Added dynamic flags to coaster-card

--- views/components/___flag.php
+++ views/components/___flag.php
@@ -1,29 +1,42 @@
 <?php
 
-/** @var array $park */
+/** @var array $coaster */
 // Im sorry, I have to use echo :(
 
-switch ($park["park_country_code"]) {
+$sql = "SELECT park_country_code FROM parks WHERE park_pk = :park_pk";
+$stmt = $_db->prepare($sql);
+$stmt->execute([":park_pk" => $coaster["coaster_park_fk"]]);
+$coaster_country_code = $stmt->fetchColumn();
+
+switch ($coaster_country_code) {
+    case "AT":
+        echo "<img src='/static/assets/icons/flag_At.svg' alt='Austria' class='object-cover aspect-square rounded-full'>";
+        break;
+
+    case "DE":
+        echo "<img src='/static/assets/icons/flag_DE.svg' alt='Germany' class='object-cover aspect-square rounded-full'>";
+        break;
+
+    case "DK":
+        echo "<img src='/static/assets/icons/flag_DK.svg' alt='Denmark' class='object-cover aspect-square rounded-full'>";
+        break;
+
+    case "FR":
+        echo "<img src='/static/assets/icons/flag_FR.svg' alt='France' class='object-cover aspect-square rounded-full'>";
+        break;
+
     case "NL":
-        _("netherlands");
-        break;
-    case 1:
-        echo ("<span class='text-(--pure-star)'>&#x2605;</span> &#x2605; &#x2605; &#x2605; &#x2605;");
+        echo "<img src='/static/assets/icons/flag_NL.svg' alt='Netherlands' class='object-cover aspect-square rounded-full'>";
         break;
 
-    case 2:
-        echo ("<span class='text-(--pure-star)'>&#x2605; &#x2605;</span> &#x2605; &#x2605; &#x2605;");
+    case "PL":
+        echo "<img src='/static/assets/icons/flag_PL.svg' alt='Poland' class='object-cover aspect-square rounded-full'>";
         break;
 
-    case 3:
-        echo ("<span class='text-(--pure-star)'>&#x2605; &#x2605; &#x2605;</span> &#x2605; &#x2605;");
+    case "SE":
+        echo "<img src='/static/assets/icons/flag_SE.svg' alt='Sweden' class='object-cover aspect-square rounded-full'>";
         break;
 
-    case 4:
-        echo ("<span class='text-(--pure-star)'>&#x2605; &#x2605; &#x2605; &#x2605;</span> &#x2605;");
-        break;
-
-    case 5:
-        echo ("<span class='text-(--pure-star)'>&#x2605; &#x2605; &#x2605; &#x2605; &#x2605;</span>");
-        break;
+    default:
+        _("");
 }
--- views/components/__coaster-card.php
+++ views/components/__coaster-card.php
@@ -1,20 +1,25 @@
 <?php
 
 /** @var array $coaster */
-$stmt = "SELECT park_country_code FROM park WHERE :park_";
+
 ?>
 
 <a class="overflow-hidden shadow-sm rounded-md w-full anim-slide-in min-h-fit coaster-card bg-(--pure-eggshell)" href="/coasters?coaster=<?php _($coaster["coaster_pk"]) ?>">
-    <article class=" w-full group">
+    <article class=" w-full group relative">
         <img class="block rounded-t-md w-full h-54 object-cover group-hover:scale-103 duration-200 bg-(--pure-indigo)"
             src="<?php _($coaster["coaster_image_path"] ?: "/static/assets/images/coaster-placeholder.webp") ?>"
             onerror="this.onerror=null; this.src='/static/assets/images/coaster-placeholder.webp'"
             alt="Coaster">
+        <div class="w-5 aspect-square rounded-full absolute top-2 right-2 bg-transparent shadow-md shadow-black/80">
+            <?php require __DIR__ . "/___flag.php"; ?>
+        </div>
         <div class="p-4 flex flex-col gap-2">
-            <h4><?php _($coaster["coaster_title"]); ?>
+            <h4>
+                <?php _($coaster["coaster_title"]); ?>
                 <?php if ($coaster["coaster_is_operational"] == 0): ?>
                     <span class="text-(--system-failure)">(Defunct)</span>
                 <?php endif; ?>
+
             </h4>
             <p class="small text-(--light-indigo)!"><?php _($coaster["coaster_model"]); ?></p>
         </div>
